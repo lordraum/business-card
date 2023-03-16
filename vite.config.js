f15e131351faee6f19bba121ec8021e3c69d506e
David Gomez <lordraum@gmail.com>
2023-03-16 16:01:22 -0500

    Correción path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/lordraum/business-card/",
+  base: "/business-card/",
 });
